b3989b1c07dd397a91ba923193449922d7adcd06
Alex Fell <alex@alexfell.com>
2011-07-14 12:56:28 +0100

    updated evernote test

--- ruby/evernote_test.rb
+++ ruby/evernote_test.rb
@@ -25,3 +25,48 @@ notebooks = note_store.listNotebooks(auth_token)
 puts "Found #{notebooks.size} notebooks:"
 default_notebook = notebooks[0]
 notebooks.each { |notebook| puts "  * #{notebook.name}"}
+
+
+puts default_notebook.guid;
+
+class NoteFilter
+   include ::Thrift::Struct, ::Thrift::Struct_Union
+   ORDER = 1
+   ASCENDING = 2
+   WORDS = 3
+   NOTEBOOKGUID = 4
+   TAGGUIDS = 5
+   TIMEZONE = 6
+   INACTIVE = 7
+
+   FIELDS = {
+     ORDER => {:type => ::Thrift::Types::I32, :name => 'order', :optional => true},
+     ASCENDING => {:type => ::Thrift::Types::BOOL, :name => 'ascending', :optional => true},
+     WORDS => {:type => ::Thrift::Types::STRING, :name => 'words', :optional => true},
+     NOTEBOOKGUID => {:type => ::Thrift::Types::STRING, :name => 'notebookGuid', :optional => true},
+     TAGGUIDS => {:type => ::Thrift::Types::LIST, :name => 'tagGuids', :element => {:type => ::Thrift::Types::STRING}, :optional => true},
+     TIMEZONE => {:type => ::Thrift::Types::STRING, :name => 'timeZone', :optional => true},
+     INACTIVE => {:type => ::Thrift::Types::BOOL, :name => 'inactive', :optional => true}
+   }
+
+   def struct_fields; FIELDS; end
+
+   def validate
+   end
+
+   ::Thrift::Struct.generate_accessors self
+ end
+
+filter = NoteFilter.new;
+filter.notebookGuid = default_notebook.guid;
+
+
+notes = note_store.findNotes(auth_token, filter, 0, 100)
+
+notes.notes.each { |note|
+  
+ content = note_store.getNote(auth_token, note.guid, true, false, false, false)
+ 
+ puts content.content
+ 
+}
